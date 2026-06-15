7fcddb5445953b0643e6d2365b242cd0044985a5
VasanthXavier <xavier96vasanth@gmail.com>
2026-06-15 12:04:11 +0530

    feat: added calculator utility

---
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        int value = calculator.add(4, 3);
+        assertEquals(7, value);
+    }
+
+    @Test
+    void testSubtract() {
+        int value = calculator.subtract(10, 3);
+        assertEquals(7, value);
+    }
+}
